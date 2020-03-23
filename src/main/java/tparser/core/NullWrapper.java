70bd6dd4e415b302c01341bafccd97a6a21248a4
VirtaulDruid <VirtualDruid70@gmail.com>
2020-03-24 02:58:25 +0800

    fix NullPointerException when root is StructPlaceHolderVisitor and the next ElementStep selects null

--- src/main/java/tparser/core/NullWrapper.java
+++ src/main/java/tparser/core/NullWrapper.java
@@ -10,6 +10,7 @@ import java.util.Objects;
  */
 final class NullWrapper extends Element {
     private static final String TAG = "null";
+
     private NullWrapper(Element parent) {
         super(TAG);
         this.setParentNode(parent);
@@ -34,7 +35,7 @@ final class NullWrapper extends Element {
 
     static boolean isNullRepresent(Element test) {
         //null param also return false
-        return test instanceof NullWrapper;
+        return test instanceof NullWrapper || test == null;
     }
 
     static Element nullRepresent(Element parent) {
